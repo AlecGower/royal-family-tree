2c1d005404c34965ddb805bab4b0b59390a721b3
Alexander Gower <alecgower@hotmail.co.uk>
2025-07-07 15:05:44 +0000

    Now extracting countries and storing them as place of birth. And also added subclasses to FOAF person for man and woman, to test heirarchical structure in learning.

--- scripts/ged_to_ttl.py
+++ scripts/ged_to_ttl.py
@@ -1,11 +1,51 @@
+import sys
+
 from gedcom.element.individual import IndividualElement
 from gedcom.parser import Parser
 
 from rdflib import Graph, URIRef, Namespace, Literal
 from rdflib.namespace import RDF, RDFS, FOAF, XSD
 
+from pycountry import countries, historic_countries, subdivisions
+
+from tqdm.auto import tqdm
+
+from urllib.error import HTTPError
+
 REL = Namespace("http://purl.org/vocab/relationship/")
 ROYALS = Namespace("http://example.org/royals/")
+SCHEMA = Namespace("http://schema.org/")
+
+
+def find_country_from_pob(pob, verbose=False):
+    search_term = pob.split(",")[-1].strip(" ,.:;-")
+    if search_term == "":
+        return None
+    try:
+        res = countries.search_fuzzy(search_term)
+        country = res[0].name
+        if verbose:
+            print(f"{search_term} -> {country} ({pob})", file=sys.stderr)
+        return country
+    except (LookupError, AttributeError):
+        try:
+            res = subdivisions.search_fuzzy(search_term)
+            country = res[0].country.name
+            if verbose:
+                print(f"{search_term} -> {country} ({pob})", file=sys.stderr)
+            return country
+        except (LookupError, AttributeError):
+            try:
+                res = historic_countries.search_fuzzy(search_term)
+                country = res[0].name
+                if verbose:
+                    print(f"{search_term} -> {country} ({pob})",
+                          file=sys.stderr)
+                return country
+            except (LookupError, AttributeError):
+                if verbose:
+                    print(f"{search_term} -> {None} ({pob})", file=sys.stderr)
+                return None
 
 
 class RoyalsGraph(Graph):
@@ -13,22 +53,102 @@ class RoyalsGraph(Graph):
 
     def __init__(self):
         super().__init__()
-        self.gender_lookup = {'M': 'male', 'F': 'female'}
+        self.bind("rel", REL)
+        self.bind("royals", ROYALS)
+        self.bind("schema", SCHEMA)
+        # self.gender_lookup = {'M': 'male', 'F': 'female'}
+        self.gender_lookup = {'M': ROYALS["Man"], 'F': ROYALS["Woman"]}
+        self.countries = []
+
+        self.parse_ontology_structures({
+            'foaf': 'http://xmlns.com/foaf/spec/index.rdf',
+            'schema': 'https://schema.org/version/latest/schemaorg-current-https.ttl',
+            'rel': 'https://vocab.org/relationship/rel-vocab-20100607.rdf'
+        })
+
+        self.extend_foaf()
+
+    def parse_ontology_structures(self, ontology_urls: dict, verbose=False):
+
+        for ns in list(self.namespaces()):
+            prefix = ns[0]
+            url = ontology_urls.get(prefix)
+            if url is not None:
+                try:
+                    self.parse(url, format='xml' if url.endswith(
+                        '.rdf') else 'turtle')
+                except HTTPError as e:
+                    print(f"{e} : {url}")
+                    raise
+            else:
+                if verbose:
+                    print(
+                        f"Namespace {ns} not found in ontologies.", file=sys.stderr)
+                continue
+
+    def extend_foaf(self):
+        self.add((ROYALS["Man"], RDFS.subClassOf, FOAF.Person))
+        self.add((ROYALS["Man"], RDFS.label,
+                 Literal("Man", datatype=XSD.string)))
+        self.add((ROYALS["Woman"], RDFS.subClassOf, FOAF.Person))
+        self.add((ROYALS["Woman"], RDFS.label,
+                 Literal("Woman", datatype=XSD.string)))
 
     def add_individual(self, ind):
         """Add an individual to the graph."""
         id = ind.get_pointer().strip('@')
         uri = ROYALS[id]
 
-        self.add((uri, RDF.type, FOAF.Person))
-        self.add((uri, FOAF.name, Literal(ind.get_name()[
-                 0] + " " + ind.get_name()[1], datatype=XSD.string)))
-        self.add((uri, FOAF.givenName, Literal(
-            ind.get_name()[0], datatype=XSD.string)))
-        self.add((uri, FOAF.familyName, Literal(
-            ind.get_name()[1], datatype=XSD.string)))
-        self.add((uri, FOAF.gender, Literal(self.gender_lookup.get(
-            element.get_gender(), 'unknown'), datatype=XSD.string)))
+        # TODO Insteaed of using gender as a string, create a subclass
+        # structure so there is more of a heirarchy
+        self.add((uri, RDF.type, self.gender_lookup.get(
+            element.get_gender(), FOAF.Person)))
+
+        givenName = ind.get_name()[0].strip()
+        familyName = ind.get_name()[1].strip()
+        fullName = "" if givenName + \
+            familyName == "" else givenName if familyName == "" else familyName if givenName == "" else givenName + " " + familyName
+        if givenName != "":
+            self.add((uri, FOAF.givenName, Literal(
+                givenName, datatype=XSD.string)))
+        if familyName != "":
+            self.add((uri, FOAF.familyName, Literal(
+                familyName, datatype=XSD.string)))
+        if fullName != "":
+            self.add((uri, FOAF.name, Literal(fullName, datatype=XSD.string)))
+
+        country = find_country_from_pob(ind.get_birth_place())
+        if country is not None:
+            country_uri = self.add_country(country)
+            if country_uri is not None:
+                self.add((uri, SCHEMA.birthPlace, country_uri))
+
+        return uri
+
+    def add_country(self, country):
+        if country == "":
+            return None
+        else:
+            try:
+                tc = 0
+                for t in self.triples((None, RDFS.label, Literal(country, datatype=XSD.string))):
+                    tc += 1
+                    assert tc < 2
+                    if len(
+                            list(self.triples((t[0], RDF.type, SCHEMA.Country)))) == 1:
+                        country_uri = t[0]
+                    else:
+                        assert False
+                country_uri
+            except (AssertionError, UnboundLocalError):
+                id = f"C{len(self.countries)+1:0>3}"
+                country_uri = ROYALS[id]
+
+                self.add((country_uri, RDF.type, SCHEMA.Country))
+                self.add((country_uri, RDFS.label, Literal(
+                    country, datatype=XSD.string)))
+                self.countries.append(country_uri)
+            return country_uri
 
 
 if __name__ == "__main__":
@@ -45,15 +165,12 @@ if __name__ == "__main__":
 
     # Create graph
     g = RoyalsGraph()
-    g.bind("rel", REL)
-    g.bind("royals", ROYALS)
 
     # Iterate through all root child elements
-    for element in elements:
+    for element in tqdm(elements):
         # Is the `element` an actual `IndividualElement`? (Allows usage of extra functions such as `surname_match` and `get_name`.)
         if isinstance(element, IndividualElement):
-            g.add_individual(element)
-            element_uri = ROYALS[element.get_pointer().strip('@')]
+            element_uri = g.add_individual(element)
 
             # Get parents of the individual
             for parent in gedcom_parser.get_parents(element):
